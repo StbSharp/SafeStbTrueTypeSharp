d37f73076438eae0b3405a7f288725de5c8db5a4
Roman Shapiro <eleroman@mail.ru>
2020-02-09 01:30:45 +0700

    First commit

---
+++ samples/StbTrueTypeSharp.MonoGame.Test/FontBaker.cs
@@ -0,0 +1,92 @@
+﻿using StbTrueTypeSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StbSharp.MonoGame.Test
+{
+	public class FontBaker
+	{
+		private byte[] _bitmap;
+		private StbTrueType.stbtt_pack_context _context;
+		private Dictionary<int, GlyphInfo> _glyphs;
+		private int bitmapWidth, bitmapHeight;
+
+		public void Begin(int width, int height)
+		{
+			bitmapWidth = width;
+			bitmapHeight = height;
+			_bitmap = new byte[width * height];
+			_context = new StbTrueType.stbtt_pack_context();
+
+			StbTrueType.stbtt_PackBegin(_context, _bitmap, width, height, width, 1);
+
+			_glyphs = new Dictionary<int, GlyphInfo>();
+		}
+
+		public void Add(byte[] ttf, float fontPixelHeight,
+			IEnumerable<CharacterRange> characterRanges)
+		{
+			if (ttf == null || ttf.Length == 0)
+				throw new ArgumentNullException(nameof(ttf));
+
+			if (fontPixelHeight <= 0)
+				throw new ArgumentOutOfRangeException(nameof(fontPixelHeight));
+
+			if (characterRanges == null)
+				throw new ArgumentNullException(nameof(characterRanges));
+
+			if (!characterRanges.Any())
+				throw new ArgumentException("characterRanges must have a least one value.");
+
+			var fontInfo = new StbTrueType.stbtt_fontinfo();
+			if (StbTrueType.stbtt_InitFont(fontInfo, ttf, 0) == 0)
+				throw new Exception("Failed to init font.");
+
+			var scaleFactor = StbTrueType.stbtt_ScaleForPixelHeight(fontInfo, fontPixelHeight);
+
+			int ascent, descent, lineGap;
+			StbTrueType.stbtt_GetFontVMetrics(fontInfo, out ascent, out descent, out lineGap);
+
+			foreach (var range in characterRanges)
+			{
+				if (range.Start > range.End)
+					continue;
+
+				var cd = new StbTrueType.stbtt_packedchar[range.End - range.Start + 1];
+				for(var i = 0; i < cd.Length; ++i)
+				{
+					cd[i] = new StbTrueType.stbtt_packedchar();
+				}
+				StbTrueType.stbtt_PackFontRange(_context, ttf, 0, fontPixelHeight,
+					range.Start,
+					range.End - range.Start + 1,
+					cd);
+
+				for (var i = 0; i < cd.Length; ++i)
+				{
+					var yOff = cd[i].yoff;
+					yOff += ascent * scaleFactor;
+
+					var glyphInfo = new GlyphInfo
+					{
+						X = cd[i].x0,
+						Y = cd[i].y0,
+						Width = cd[i].x1 - cd[i].x0,
+						Height = cd[i].y1 - cd[i].y0,
+						XOffset = (int)cd[i].xoff,
+						YOffset = (int)Math.Round(yOff),
+						XAdvance = (int)Math.Round(cd[i].xadvance)
+					};
+
+					_glyphs[i + range.Start] = glyphInfo;
+				}
+			}
+		}
+
+		public FontBakerResult End()
+		{
+			return new FontBakerResult(_glyphs, _bitmap, bitmapWidth, bitmapHeight);
+		}
+	}
+}
---
+++ src/StbTrueTypeSharp/StbTrueType.cs
@@ -0,0 +1,35 @@
+﻿namespace StbTrueTypeSharp
+{
+#if !STBSHARP_INTERNAL
+	public
+#else
+	internal
+#endif
+	static partial class StbTrueType
+	{
+		public static uint stbtt__find_table(FakePtr<byte> data, uint fontstart, string tag)
+		{
+			int num_tables = ttUSHORT(data + fontstart + 4);
+			var tabledir = fontstart + 12;
+			int i;
+			for (i = 0; i < num_tables; ++i)
+			{
+				var loc = (uint)(tabledir + 16 * i);
+				if ((data + loc + 0)[0] == tag[0] && (data + loc + 0)[1] == tag[1] &&
+					(data + loc + 0)[2] == tag[2] && (data + loc + 0)[3] == tag[3])
+					return ttULONG(data + loc + 8);
+			}
+
+			return 0;
+		}
+
+		public static void stbtt__dict_get_ints(stbtt__buf b, int key, out uint _out_)
+		{
+			var temp = new FakePtr<uint>(new uint[1]);
+
+			stbtt__dict_get_ints(b, key, 1, temp);
+
+			_out_ = temp[0];
+		}
+	}
+}
